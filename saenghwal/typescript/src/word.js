72b432ee0509b6c532dbd7ef6e28dd0aca7ecd3a
Espebaum/gyopark <vocops5000@gmail.com>
2023-10-15 19:30:35 +0900

    typescript

---
+++ saenghwal/typescript/src/word.js
@@ -0,0 +1,3 @@
+var arr1 = [1, 2, 3];
+var arr2 = [1, 2, 3]; // 선언 Array<T>
+var tuple = ['Hello', 42, true];
---
+++ saenghwal/typescript/src/word.ts
@@ -0,0 +1,3 @@
+let arr1: number[] = [1, 2, 3];
+let arr2: Array<number> = [1, 2, 3]; // 선언 Array<T>
+let tuple: [string, number, boolean] = ['Hello', 42, true];
