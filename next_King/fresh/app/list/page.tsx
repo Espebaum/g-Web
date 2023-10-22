28c27a6b8c55fef6a479958caa517bfedd8c6d6f
Espebaum/gyopark <vocops5000@gmail.com>
2023-10-22 20:37:23 +0900

    web

---
+++ next_King/fresh/app/list/page.tsx
@@ -0,0 +1,9 @@
+export default function List() { 
+    return (
+      <div>
+        <h1 className="title"> 상품 목록 </h1>  
+      </div>
+    )
+  }
+  
+  
---
+++ next_King/fresh/app/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() { 
+  let name = 'gyopark'
+  return (
+    <div>
+      <h1 className="title"> 애플 후레시 </h1>
+      <p className="title-sub">
+        by { name } </p>
+
+    </div>
+  )
+}
+
