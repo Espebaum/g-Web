de964e520baba360669cb1913377ddc12bf2b382
Espebaum/gyopark <vocops5000@gmail.com>
2023-10-23 20:45:10 +0900

    next js

--- next_King/fresh/app/cart/page.tsx
+++ next_King/fresh/app/cart/page.tsx
@@ -1,25 +1,52 @@
+import React from 'react'
 import Link from "next/Link"
+import HelloComponent from './data'
 
-export default function Cart() {
+export default function Cart() { // 부모 컴포넌트
+  let MyCart: string[] = ['Tomatoes', 'Pasta']
   return (
     <div>
       <h4 className="title">Cart</h4>
-      <div className="cart-item">
-        <p>상품명</p>
-        <p>$40</p>
-        <p>1개</p>
-      </div>
-      <div className="cart-item">
-        <p>상품명</p>
-        <p>$40</p>
-        <p>1개</p>
-      </div>
-      <div className="payment">
-        <p>
-        <Link href="/cart/payment">결제하기</Link>
-        </p>
-      </div>
-    </div>
+      <HelloComponent/>
+      <CartItem item={MyCart[0]} />
+      <CartItem item={MyCart[1]} />
+      <CartItem />
+      <Payment/>
+      <Banner Card="롯데카드"/>
+      <Button Color="blue"/>
+    </div> 
   )
 } 
-  
+
+function Button(props) {
+  return <button style={{ 
+      background : props.Color, 
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      width: '100px',
+      height: '40px',
+    }}>Push This</button>
+}
+
+function Banner(props) {
+  return (<h5 className='title'> {props.Card} 결제 행사 중</h5>)
+}
+
+function CartItem(props) { // 자식 컴포넌트
+  return (
+    <div className="cart-item">
+      <p>{props.item}</p>
+      <p>$40</p>
+      <p>1개</p>
+    </div>
+  )
+}
+
+function Payment() {
+  return (
+    <div className="payment">
+      <p><Link href="/cart/payment">결제하기</Link></p>
+    </div>
+  )
+}
--- next_King/fresh/app/list/page.tsx
+++ next_King/fresh/app/list/page.tsx
@@ -1,15 +1,43 @@
-export default function List() { 
-  let 상품 = ['Tomatoes', 'Pasta', 'Coconut']
+'use client'
 
+import { useState } from 'react'
+
+export default function List() { 
+  let items = ['Tomatoes', 'Pasta', 'Coconut']
+  let [num, setNum] = useState([0, 0, 0]) // client component에서만 사용가능
   return (
       <div>
         <h1 className="title"> 상품 목록 </h1>
+        
+        {/* <span>{num[0]}</span>
+        <button onClick={()=>{ 
+            let copy = [...num];
+            copy[0]++
+            setNum(copy) 
+          }}>+</button>
+      
+        <span>{num[1]}</span>
+        <button>+</button>        
+        <span>{num[2]}</span>
+        <button>+</button> */}
+        
         {
-          상품.map((a, i)=>{
+          items.map((a, i)=>{
             return (
               <div className="food" key={i}>
                 <img src={ `food${i}.png` } className="food-img"/>
                 <h4> { a } $40 </h4>
+                <span> { num[i] } </span>
+                <button onClick={()=>{ 
+                  let copy = [...num];
+                  copy[i]++
+                  setNum(copy) 
+                  }}> + </button>
+                <button onClick={()=>{ 
+                  let copy = [...num];
+                  copy[i]--
+                  setNum(copy) 
+                  }}> - </button>
               </div>
             )
           })
