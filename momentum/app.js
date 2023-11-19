76d4a56478108aff1656c319e386597f79b1ab52
Espebaum/gyopark <vocops5000@gmail.com>
2023-11-19 21:50:04 +0900

    momemtum

--- momentum/app.js
+++ momentum/app.js
@@ -1,3 +1,15 @@
-const title = document.getElementsByTagName('h1');
+const h1 = document.querySelector(".hello h1")
 
-console.log(typeof title);
+h1.addEventListener("click", () => {
+    h1.classList.toggle("active");
+}) // active가 h1의 classList에 존재한다면 제거하고, 그렇지 않다면 추가한다.
+
+h1.addEventListener("mouseenter", () => {
+    h1.textContent = "Mouse is here!"
+})
+
+h1.addEventListener("mouseleave", () => {
+    h1.textContent = "Oh my God!"
+})
+
+// console.log(h1);
