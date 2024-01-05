c930b5d202f155014ae55e42c6dcb26cfd1ee48b
Espebaum/gyopark <vocops5000@gmail.com>
2024-01-05 20:08:00 +0900

    ..

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,12 +3,21 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+function formatName(user) {
+    return user.firstName + ' ' + user.lastName;
+}
 
+const user = {
+    firstName: 'Harper',
+    lastName: 'Perez',
+};
+
+const element = <h1>Hello, {formatName(user)}!</h1>;
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <element />
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
