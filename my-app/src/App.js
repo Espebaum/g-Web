ffcd2c3578ead197157972cd54841d9e58f0e486
Espebaum/gyopark <vocops5000@gmail.com>
2024-01-07 20:45:23 +0900

    16_useEffect

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,70 @@
 import './App.css';
+import React from 'react';
+import { useState, useRef } from 'react';
 import Hello from './Hello';
 import Wrapper from './Wrapper';
 import Counter from './Counter';
 import InputSample from './InputSample';
 import UserList from './UserList';
-// import { useState } from 'react';
+import CreateUser from './CreateUser';
 
 function App() {
+    const [inputs, setInputs] = useState({
+        username: '',
+        email: '',
+    });
+    const { username, email } = inputs;
+    const onChange = (e) => {
+        const { name, value } = e.target;
+        setInputs({
+            ...inputs,
+            [name]: value,
+        });
+    };
+    const [users, setUsers] = useState([
+        {
+            id: 1,
+            username: 'velopert',
+            email: 'public.velopert@gmail.com',
+            active: true,
+        },
+        {
+            id: 2,
+            username: 'tester',
+            email: 'tester@example.com',
+            active: false,
+        },
+        {
+            id: 3,
+            username: 'liz',
+            email: 'liz@example.com',
+            active: false,
+        },
+    ]);
+
+    const nextId = useRef(4);
+    const onCreate = () => {
+        const user = {
+            id: nextId.current,
+            username,
+            email,
+        };
+        setUsers(users.concat(user));
+        setInputs({
+            username: '',
+            email: '',
+        });
+        nextId.current += 1;
+    };
+
+    const onRemove = (id) => {
+        setUsers(users.filter((user) => user.id !== id));
+    };
+
+    const onToggle = (id) => {
+        setUsers(users.map((user) => (user.id === id ? { ...user, active: !user.active } : user)));
+    };
+
     return (
         <>
             <Wrapper>
@@ -14,8 +72,12 @@ function App() {
                 <Hello color="pink"></Hello>
             </Wrapper>
             <Counter />
+            <br />
             <InputSample />
-            <UserList />
+            <br />
+            <CreateUser username={username} email={email} onChange={onChange} onCreate={onCreate} />
+            <br />
+            <UserList users={users} onRemove={onRemove} onToggle={onToggle} />
         </>
     );
 }
--- my-app/src/UserList.js
+++ my-app/src/UserList.js
@@ -1,38 +1,34 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 // 11. 배열 렌더링하기
 // 09. 배열 내장함수들 (foreach, map, indexOf, findIndex, find, filter, splice ...)
 
-function User({ user }) {
+function User({ user, onRemove, onToggle }) {
+    useEffect(() => {
+        console.log('나타남');
+        console.log(user);
+        return () => {
+            console.log('사라짐');
+            console.log(user);
+        };
+    }, [user]);
+
     return (
         <div>
-            <b>{user.username}</b> <span>({user.email})</span>
+            <b style={{ cursor: 'pointer', color: user.active ? 'green' : 'black' }} onClick={() => onToggle(user.id)}>
+                {user.username}
+            </b>{' '}
+            <span>({user.email})</span>
+            <button onClick={() => onRemove(user.id)}>삭제</button>
         </div>
     );
 }
 
-function UserList() {
-    const users = [
-        {
-            id: 1,
-            username: 'velopert',
-            email: 'public.velopert@gmail.com',
-        },
-        {
-            id: 2,
-            username: 'tester',
-            email: 'tester@example.com',
-        },
-        {
-            id: 3,
-            username: 'liz',
-            email: 'liz@example.com',
-        },
-    ];
+function UserList({ users, onRemove, onToggle }) {
     return (
         <div>
-            {users.map((user, index) => (
-                <User user={user} key={index} />
+            {users.map((user) => (
+                <User user={user} key={user.id} onRemove={onRemove} onToggle={onToggle} />
             ))}
         </div>
     );
