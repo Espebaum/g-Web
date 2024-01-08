2111f7200e5bece65e722356a866c47bb0452a6c
Espebaum/gyopark <vocops5000@gmail.com>
2024-01-08 15:44:27 +0900

    style

--- rr/src/pages/MainPage.js
+++ rr/src/pages/MainPage.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './MainPage.css';
 
 function MainPage() {
     return (
-        <div>
-            <p>Hello World!</p>
-            <Link to="/login">로그인</Link>
+        <div className="main-container">
+            <p className="card">Solve Problems!</p>
+            <Link to="/login" className="btn">
+                로그인
+            </Link>
         </div>
     );
 }
